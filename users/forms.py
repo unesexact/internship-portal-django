97cca17936e3ec38861a1b44e66cdb6228df4ab0
unesexact <oukessouyounes1@gmail.com>
2026-06-10 13:15:10 +0100

    Improve registration form validation UI and Bootstrap styling

--- users/forms.py
+++ users/forms.py
@@ -8,6 +8,29 @@ class RegisterForm(UserCreationForm):
         choices=[("student", "Student"), ("company", "Company")]
     )
 
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Username"}
+        )
+    )
+
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={"class": "form-control", "placeholder": "Password"}
+        )
+    )
+
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={"class": "form-control", "placeholder": "Confirm Password"}
+        )
+    )
+
+    user_type = forms.ChoiceField(
+        choices=[("student", "Student"), ("company", "Company")],
+        widget=forms.Select(attrs={"class": "form-select"}),
+    )
+
     class Meta:
         model = User
         fields = ["username", "password1", "password2", "user_type"]
